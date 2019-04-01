d47e026d8a0968cab5e6d55a76c8d2b5fa0adb4a
David Charles <ratlover03@gmail.com>
2019-04-01 13:16:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ require_relative '../config/environment'
       id = row[0]
       name = row[1]
       breed = row[2]
-      new_dog = se;f.new(id: id, name: name, breed: breed)
+      new_dog = self.new(id: id, name: name, breed: breed)
       new_dog
   end
 
